42632616fc5cc21f3cdbdd38f37dab1a3e10d163
Raffayl <120395819+Raffayl@users.noreply.github.com>
2023-01-19 16:00:17 +0100

    update

--- EVALUATION_CORRECTION/src/evaluation_correction/App.java
+++ EVALUATION_CORRECTION/src/evaluation_correction/App.java
@@ -7,30 +7,30 @@ public class App {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int mois;
-		int[] daysInMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int[] joursdanslemois = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		do {
-			System.out.print("Entrez un numéro de mois (1-12): ");
+			System.out.print("Entrez un numéro de mois de 1 a 12): ");
 			mois = sc.nextInt();
-			String[] monthNames = new String[13];
-			monthNames[0] = "raté";
-			monthNames[1] = "janvier";
-			monthNames[2] = "fevrier";
-			monthNames[3] = "mars";
-			monthNames[4] = "avril";
-			monthNames[5] = "mai";
-			monthNames[6] = "juin";
-			monthNames[7] = "juillet";
-			monthNames[8] = "aout";
-			monthNames[9] = "septembre";
-			monthNames[10] = "octobre";
-			monthNames[11] = "novembre";
-			monthNames[12] = "decembre";
-			System.out.println("Le mois sélectionné est : " + monthNames[mois - 1]);
-		} while (mois < 1 || mois > 13);
+			String[] nomdumois = new String[12];
+			nomdumois[0] = "janvier";
+			nomdumois[1] = "fevrier";
+			nomdumois[2] = "mars";
+			nomdumois[3] = "avril";
+			nomdumois[4] = "mai";
+			nomdumois[5] = "juin";
+			nomdumois[6] = "juillet";
+			nomdumois[7] = "aout";
+			nomdumois[8] = "septembre";
+			nomdumois[9] = "octobre";
+			nomdumois[10] = "novembre";
+			nomdumois[11] = "decembre";
+
+			System.out.println("Le mois sélectionné est : " + nomdumois[mois - 1]);
+		} while (mois < 1 || mois > 12);
 
 		if (mois % 2 == 0) {
 			System.out.print("Nombres de jours impairs: ");
-			for (int i = 1; i <= daysInMonth[mois - 1]; i++) {
+			for (int i = 1; i <= joursdanslemois[mois - 1]; i++) {
 				if (i % 2 != 0) {
 					System.out.print(i + " ");
 				}
