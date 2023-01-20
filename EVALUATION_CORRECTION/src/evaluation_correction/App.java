dbbdb75ca777c88d4ee109a4b1fe68d26aa9971d
Raffayl <120395819+Raffayl@users.noreply.github.com>
2023-01-20 08:42:16 +0100

    Update App.java

--- EVALUATION_CORRECTION/src/evaluation_correction/App.java
+++ EVALUATION_CORRECTION/src/evaluation_correction/App.java
@@ -1,16 +1,27 @@
 package evaluation_correction;
 
+//Importation de la classe Scanner
 import java.util.Scanner;
 
 public class App {
-
+	
+		//Définir la fonction principale
 	public static void main(String[] args) {
+		
+		//Créer un objet de type Scanner pour récupérer les entrées utilisateur
 		Scanner sc = new Scanner(System.in);
+		
+		//Déclarer une variable "mois"
 		int mois;
+		
+		////Déclarer un tableau "joursdanslemois" contenant les nombres de jours de chaque mois
 		int[] joursdanslemois = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		do {
+			
 			System.out.print("Entrez un numéro de mois de 1 a 12): ");
 			mois = sc.nextInt();
+			
+			//Déclarer un tableau "nomdumois" contenant les noms des mois
 			String[] nomdumois = new String[12];
 			nomdumois[0] = "janvier";
 			nomdumois[1] = "fevrier";
@@ -25,9 +36,17 @@ public class App {
 			nomdumois[10] = "novembre";
 			nomdumois[11] = "decembre";
 
-			System.out.println("Le mois sélectionné est : " + nomdumois[mois - 1]);
-		} while (mois < 1 || mois > 12);
+			System.out.println("Le mois sélectionné est : " + nomdumois[mois - 1]);	
+		}
+		//Faire une boucle pour répéter les étapes suivantes :
+		while (mois < 1 || mois > 12);
 
+		/*Si le mois saisi est pair :
+		Afficher "Nombres de jours impairs :"
+		Faire une boucle pour parcourir les jours du mois saisi
+		Si le jour est impair, l'afficher
+		 */
+		
 		if (mois % 2 == 0) {
 			System.out.print("Nombres de jours impairs: ");
 			for (int i = 1; i <= joursdanslemois[mois - 1]; i++) {
@@ -35,6 +54,11 @@ public class App {
 					System.out.print(i + " ");
 				}
 			}
+                 /*Sinon (si le mois saisi est impair) :
+				   Afficher "Nombres de jours pairs (sauf ceux qui se terminent par 0) :"
+				   Faire une boucle pour parcourir les jours du mois saisi
+			       Si le jour est pair et ne se termine pas par 0, l'afficher
+                */
 		} else {
 			System.out.print("Nombres de jours pairs (sauf ceux qui se terminent par 0): ");
 			for (int i = 1; i <= 31; i++) {
